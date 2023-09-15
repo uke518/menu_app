05344eb77e5ecaa94d787df8e0aad81d021deba8
RyomaAdachi <ryomaadaccho6@gmail.com>
2023-09-15 21:58:19 +0900

    requirment.txt

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import openai
+import unicodedata
 
-OPEN_AI_API_KEY = ''
+OPEN_AI_API_KEY = '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'
 app = Flask(__name__)
 app.json.ensure_ascii = False
 
 @app.route('/', methods=['POST'])
 def index():
     meal_type = request.form.get('meal_type')
-    dish_num = request.form.get('dish_num')
-    tastes = request.form.get('tastes')
-    main_dish = request.form.get('main_dish')
-    preference = request.form.get('preference')
+    dish_num = request.form.get('dish_num', 3)
+    tastes = request.form.get('tastes', ['辛い'])
+    main_dish = request.form.get('main_dish', '肉')
+    preference = request.form.get('preference', 'ニンジンを使いたい')
 
-    question = make_question(meal_type, dish_num, tastes, main_dish, preference)
-    gpt_answer = ask_gpt(question)
-    return gpt_answer
+    dish_list = make_dish_list(meal_type, dish_num, tastes, main_dish, preference)
+    descriptions = make_descriptions(dish_list)
+    response = []
+    for dish, description in zip(dish_list, descriptions):
+        response.append({"title": dish, "description": description})
+    return response
 
 
-def make_question(meal_type, dish_num, tastes, main_dish, preference):
-    question = f"以下の条件で健康バランスの良い{meal_type}の献立とその材料、調理手順を提案してください。。また出力はフォーマットに則って、それ以外の言葉は喋らないで。以下の条件で朝食の献立を考えてください。また出力はフォーマットに則って、それ以外の言葉は喋らないで\n。## 条件\n品数： {dish_num}\n味の好み： {tastes}\nメイン： {main_dish}\nその他要望： {preference}\n\n## フォーマット\n料理名1：〇〇\n料理名2：〇〇\n料理名3：〇〇..."
-    return question
+def make_dish_list(meal_type, dish_num, tastes, main_dish, preference):
+    question = f"以下の条件で健康バランスの良い{meal_type}の献立を提案してください。。また出力はフォーマットに則って、それ以外の言葉は喋らないで。以下の条件で朝食の献立を考えてください。また出力はフォーマットに則って、それ以外の言葉は喋らないで\n。## 条件\n品数： {dish_num}\n味の好み： {tastes}\nメイン： {main_dish}\nその他要望： {preference}\n\n## フォーマット\n料理名1：〇〇\n料理名2：〇〇\n料理名3：〇〇..."
+    menu_text = ask_gpt(question)
+    dish_list = []
+    for line in menu_text.split("\n"):
+        try:
+            print(line.split(":"))
+            dish_list.append(line.split(":")[1])
+        except:
+            break
+    dish_list = [dish.strip() for dish in dish_list]
+    return dish_list
+
+
+def make_descriptions(dish_list):
+    descriptions = []
+    for dish in dish_list:
+        question = f"{dish}の原材料と料理手順を教えて。出力はフォーマットに則って、それ以外の言葉は喋らないで。n## フォーマット\n原材料：〇〇\n手順：〇〇"
+        description = ask_gpt(question)
+        descriptions.append(description)
+    print(descriptions)
+    return descriptions
 
 
 def ask_gpt(question):
@@ -36,7 +59,7 @@ def ask_gpt(question):
     )
 
     selected_response = response.choices[0]
-    reply_text = selected_response.message.content
+    reply_text = unicodedata.normalize('NFKC', selected_response.message.content)
     return reply_text
 
 
